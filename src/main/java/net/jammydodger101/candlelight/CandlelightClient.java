dcdebd79a81b0b02f12049633f23335ae998477c
JammyDodger101 <133568549+JammyDodger101@users.noreply.github.com>
2023-12-10 09:46:44 +0000

    candlelight base

---
+++ src/main/java/net/jammydodger101/candlelight/CandlelightClient.java
@@ -0,0 +1,11 @@
+package net.jammydodger101.candlelight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CandlelightClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
