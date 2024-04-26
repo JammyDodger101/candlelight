d39bc01b625752fecb0c53dfedc0db47f2793778
JammyDodger101 <133568549+JammyDodger101@users.noreply.github.com>
2024-04-26 18:08:13 +0100

    (ver 1.3.22.9) - location, status and trapped commands using entity argument type (@a, @s, etc. functionality)

--- src/main/java/net/jammydodger101/candlelight/command/GetCandleLocationCommand.java
+++ src/main/java/net/jammydodger101/candlelight/command/GetCandleLocationCommand.java
@@ -5,17 +5,21 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.jammydodger101.candlelight.util.PlayerCandleHandler;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 
+import java.util.Locale;
+
 public class GetCandleLocationCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("candleLocation")
                 .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
-                .then(CommandManager.argument("playerName", StringArgumentType.string())
+                //.then(CommandManager.argument("playerName", StringArgumentType.string())
+                        .then(CommandManager.argument("playerName", EntityArgumentType.players())
                         .executes(GetCandleLocationCommand::run)));
 
     }
@@ -23,7 +27,8 @@ public class GetCandleLocationCommand {
     private static int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
 
         ServerPlayerEntity player = context.getSource().getPlayer();
-        String id = StringArgumentType.getString(context, "playerName");
+        //String id = StringArgumentType.getString(context, "playerName");
+        String id = EntityArgumentType.getPlayer(context, "playerName").getEntityName().toLowerCase();
 
         BlockPos pos = PlayerCandleHandler.getCandleCoordinates(id, player);
 
--- src/main/java/net/jammydodger101/candlelight/command/GetPlayerTrappedStatusCommand.java
+++ src/main/java/net/jammydodger101/candlelight/command/GetPlayerTrappedStatusCommand.java
@@ -5,16 +5,20 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.jammydodger101.candlelight.util.PlayerCandleHandler;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
+import java.util.Locale;
+
 public class GetPlayerTrappedStatusCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(CommandManager.literal("trapped")
                 .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
-                .then(CommandManager.argument("playerName", StringArgumentType.string())
+                //.then(CommandManager.argument("playerName", StringArgumentType.string())
+                        .then(CommandManager.argument("playerName", EntityArgumentType.players())
                         .executes(GetPlayerTrappedStatusCommand::run)));
 
     }
@@ -22,7 +26,7 @@ public class GetPlayerTrappedStatusCommand {
     private static int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
 
         ServerPlayerEntity player = context.getSource().getPlayer();
-        String id = StringArgumentType.getString(context, "playerName");
+        String id = EntityArgumentType.getPlayer(context, "playerName").getEntityName().toLowerCase();
 
 
         Boolean trappedStatus = PlayerCandleHandler.checkPlayerTrappedStatusCommand(id);
